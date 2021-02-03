74d0c93fa172efff7c8b353a9534507db88b984e
PavithraRamasamyR <78472840+PavithraRamasamyR@users.noreply.github.com>
2021-02-03 14:39:57 +0530

    Add files via upload

---
+++ sample1.py
@@ -0,0 +1,12 @@
+a=int( input("Enter A value"))
+b= int(input ("Enter B value"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+
+
+print("The addition of two values",c)
+print("The addition of two values",d)
+print("The addition of two values",e)
+print("The addition of two values",f)
