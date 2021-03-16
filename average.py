ca589805673311c23765b9c189c59fcb9c95c6eb
PavithraRamasamyR <78472840+PavithraRamasamyR@users.noreply.github.com>
2021-03-16 13:41:15 +0530

    average

---
+++ average.py
@@ -0,0 +1,11 @@
+Tamil=float(input("Enter the tamil mark:")
+English=float(input("Enter the english mark:")
+Maths=float(input("Enter the maths mark:")
+Science=float(input("Enter the science mark:")
+Social=float(input("Enter the social mark:")
+total=Tamil+English+Maths+Science+Social
+Average=total/5
+percentage=(total/500)*100
+print("Total Mark is:",total)
+print("Average is:",Average)
+print("percentage is:",percentage)
