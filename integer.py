5475702f5667f80010e6db4ea4057d48e61ec702
PavithraRamasamyR <78472840+PavithraRamasamyR@users.noreply.github.com>
2021-04-20 14:22:21 +0530

    integer program

---
+++ integer.py
@@ -0,0 +1,17 @@
+Name='pavi'
+Regno=183
+Department='MCA'
+name,regno,department='kavya',123,'MCA'
+tamil=maths=science=95
+print (Name)
+print (Regno)
+print (Department)
+print (name)
+print (regno)
+print (department)
+print (tamil)
+print (maths)
+print (science)
+
+
+
