b88728a34d6e7e6cfc2c19d4e4753ae7813db495
PavithraRamasamyR <78472840+PavithraRamasamyR@users.noreply.github.com>
2021-04-20 13:56:32 +0530

    student marks eg

---
+++ student1.py
@@ -0,0 +1,12 @@
+Name=(input("Enter the student name:"))
+Tamil=int(input("Enter the Tamil mark:"))
+English=int(input("Enter the English mark:"))
+Maths=int(input("Enter the Maths mark:"))
+Science=int(input("Enter the Science mark:"))
+Social=int(input("Enter the social mark:"))
+Total=Tamil+English+Maths+Science+Social
+Average=Total/5
+Percentage=(Total/500)*100
+print("The total marks is:",Total)
+print("The average is:",Average)
+print("The percentage is:",Percentage) 
