3ea64d79b497b1f3d81adf3641aa76988ab402d5
PavithraRamasamyR <78472840+PavithraRamasamyR@users.noreply.github.com>
2021-04-20 14:13:48 +0530

    logical program

---
+++ logical.py
@@ -0,0 +1,10 @@
+p=int(input("Enter the value of p:"))
+q=int(input("Enter the value of q:"))
+r=p&q
+s=p|q
+u=p^q
+v= ~p
+print(" The r value for AND:",r)
+print(" The q value for OR:",s)
+print(" The v value for EXOR:",u)
+print(" The v value for complement:",v)
