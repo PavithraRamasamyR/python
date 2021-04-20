6903a7c8991e443e30c6bf4b94461bbdfb2dede4
PavithraRamasamyR <78472840+PavithraRamasamyR@users.noreply.github.com>
2021-04-20 14:23:06 +0530

    float program using marks

---
+++ float.py
@@ -0,0 +1,26 @@
+Name='pavi'
+Regno=183
+Department='MCA'
+Mark1=45.5
+Mark2=87.4
+Mark3=89.0
+name,regno,department,mark1,mark2,mark3='kavya',123,'MCA',90.5,78.9,60.7
+tamil=maths=science=85.5
+print (Name)
+print (Regno)
+print (Department)
+print (Mark1)
+print (Mark2)
+print (Mark3)
+print (name)
+print (regno)
+print (department)
+print (mark1)
+print (mark2)
+print (mark3)
+print (tamil)
+print (maths)
+print (science)
+
+
+
