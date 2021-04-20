5013dc22f9b71d5495953cbb96db1b3872263ded
PavithraRamasamyR <78472840+PavithraRamasamyR@users.noreply.github.com>
2021-04-20 14:01:10 +0530

    string program

---
+++ string.py
@@ -0,0 +1,11 @@
+str='welcome to python'
+print (str)
+print (str[0])
+print (str[1])
+print (str[1:5])
+print (str[3:10])
+print (str[5:9])
+print (str[3:1])
+print (str[3:])
+print (str * 3)
+print (str + "class")
