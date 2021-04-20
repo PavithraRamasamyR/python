e34546c0e6c27071e6375697fe3a35d66bdfcaa8
PavithraRamasamyR <78472840+PavithraRamasamyR@users.noreply.github.com>
2021-04-20 13:46:55 +0530

    functionmark.py

---
+++ functionmark.py
@@ -0,0 +1,17 @@
+def total (m1,m2,m3,m4,m5):
+	c=m1+m2+m3+m4+m5
+	d=c/5
+	print ("Total",c)
+	print ("Average",d)
+	return;
+m1=int(input("Enter the Tamil Marks:"))
+m2=int(input("Enter the English Marks:"))
+m3=int(input("Enter the Maths Marks:"))
+m4=int(input("Enter the Science Marks:"))
+m5=int(input("Enter the Social Marks:"))
+total(m1,m2,m3,m4,m5)
+
+
+
+
+
