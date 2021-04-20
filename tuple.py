90a272d7eae0093ec09862bda1abb0dd827805c6
PavithraRamasamyR <78472840+PavithraRamasamyR@users.noreply.github.com>
2021-04-20 13:49:01 +0530

    tuple

---
+++ tuple.py
@@ -0,0 +1,11 @@
+g=(9,'red',5,'blue',9.0,'yellow')
+h=(389,'sunshine',77.3,'sunset')
+k=('beautiful',456,'desert',79.9,'sand')
+h[0]=778
+print (h)
+print (k[3])
+print (h[3:3])
+print (g[2:3])
+print (k[1:2])
+print (g[3:])
+print (k *4)
