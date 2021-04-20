ea4eafb397ff2f09606c6a5ecf5bbe081b070898
PavithraRamasamyR <78472840+PavithraRamasamyR@users.noreply.github.com>
2021-04-20 14:16:52 +0530

    list program

---
+++ list.py
@@ -0,0 +1,11 @@
+p=['name',5.0,'pavi',90,'kavi',98]
+s=[345,'sudha',9.0]
+r=['beautiful',456,'flower',79.9]
+s[0]=123
+print (p)
+print (p[0])
+print (r[2:3])
+print (s[1:1])
+print (p[3:1])
+print (s[1:])
+print (s *4)
