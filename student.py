c92d6e6235ce2aec07f8c706a76a3ab3b8fa2a54
PavithraRamasamyR <78472840+PavithraRamasamyR@users.noreply.github.com>
2021-04-20 13:58:39 +0530

    assiging mark for student marklist

---
+++ student.py
@@ -0,0 +1,11 @@
+Tamil=80
+English=90
+Maths=96
+Science=89
+Social=98
+Total=Tamil+English+Maths+Science+Social
+Average=Total/5
+Percentage=(Total/500)*100
+print("The total marks is:",Total)
+print("The average is:",Average)
+print("The percentage is:",Percentage) 
